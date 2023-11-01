efafdf78d3fa2855ffb3152f56caf7933cd02dab
Edvane Saraiva <edvanne_batista@hotmail.com>
2023-11-01 15:47:17 -0300

    Initial Commit

---
+++ AppMovies/AppMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMoviesApp.swift
+//  AppMovies
+//
+//  Created by Aron Uchoa Bruno on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AppMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
